6d6ef124a8c0731b8878912148748e04e8df5d1b
Tymur Lysenko <nanotimcool@gmail.com>
2017-12-04 00:58:01 +0200

    Finished Lab 4 and done a part of Lab 5
    Finished Lab 4 and done a part of Lab 5

--- Lab works/Lab 5/Lab 5.py
+++ Lab works/Lab 5/Lab 5.py
@@ -13,36 +13,16 @@ def multLists(leftList, rightList, listSize):
   return result
 
 # main
+xArr = [  list(range(0, 13)), list(range(12, 25)), list(range(24, 37))
+        , list(range(36, 49)), list(range(48, 61)), list(range(60, 73))
+        , list(range(72, 85)), list(range(84, 97)), list(range(96, 109))]
 
-# input values
-n = 14
+xArrStr = [  "0-12", "12-24", "24-36", "36-48", "48-60"
+           , "60-72", "72-84", "84-96", "96-108"]
 
-# Empirical ditribution
-xArr = [list(range(0, 13)), list(range(12, 25)), list(range(24, 37))
-      , list(range(36, 49)), list(range(48, 61)), list(range(60, 73))
-      , list(range(72, 85)), list(range(84, 97)), list(range(96, 109))]
+mArr = [16, 12, 12, 9, 8, 8, 6, 6, 4]
 
-xArrStr = ["0-12", "12-24", "24-36", "36-48", "48-60"
-         , "60-72", "72-84", "84-96", "96-108"]
-
-mArr = [6 + n, 5 + n, 5 + n, 5 + n, 5 + n, 5 + n, 4 + n, 5 + n, 4 + n]
-
-empDict = dict()
-
-for i in range(0, len(xArrStr)):
-  empDict[xArrStr[i]] = set()
-  empDict[xArrStr[i]] = mArr[i]
-
-empTab = tt.Texttable()
-
-empTab.add_row(["X"] + xArrStr)
-empTab.add_row(["m[i]"] + mArr)
-
-printEmpTab = empTab.draw()
-
-print(printEmpTab)
-
-# Frequency polygon
+# print frequency polygon
 xAverList = len(xArr) * [0]
 
 for i in range(0, len(xArr)):
@@ -77,13 +57,16 @@ sigma   = mt.sqrt(dispers)
 
 print("x aver = %(xa)f\nD = %(dsp)f\nsigma = %(sgm)f" % { "xa" : xAver, "dsp" : dispers, "sgm" : sigma })
 
+# lambda
+lam = 0.5 * ( (1 / xAver) + (1 / sigma) )
+print("Lambda =", lam)
+
 # Table with probabilities
+
 pTab = tt.Texttable()
 
-pTab.add_row(["X", "x[i]", "x[i + 1]", "p[i]", "theoretical m[i]", "m", "(m[t]^T - m) ^ 2 / m[t]^T"])
-
-probability = 1 / len(mArr)
-teoreticalM = probability * sumMArr
+pTab.add_row( ["X", "x[i]", "x[i + 1]", "-lambda[x[i]]", "-lambda[x[i + 1]]", "e ^ -lambda[x[i]]"
+             , "e ^ -lambda[x[i + 1]]", "p[i]", "theoretical m[i]", "m", "(m[t]^T - m) ^ 2 / m[t]^T"])
 
 sumLeftX  = 0
 sumRightX = 0
@@ -93,25 +76,16 @@ for i in range(0, len(xArrStr)):
   sumLeftX  += xArr[i][0]
   sumRightX += xArr[i][len(xArr[i]) - 1]
 
-  criteria = ((teoreticalM - mArr[i]) ** 2) / teoreticalM
-  criteriaSum += criteria
+  # teoreticalM = probability * sumMArr
+  # criteria = ((teoreticalM - mArr[i]) ** 2) / teoreticalM
+  # criteriaSum += criteria
 
-  pTab.add_row([xArrStr[i]] + xArrStr[i].split("-") +
-               [probability] + [teoreticalM] + [mArr[i]] +
-               [criteria])
+  pTab.add_row([xArrStr[i]] + xArrStr[i].split("-") + (6 * [""]) + # TODO : get to know how to count these lambdas, p[i]
+               [mArr[i]] + [""])
 
-pTab.add_row(["Sum"] + [sumLeftX, sumRightX
-           , len(xArrStr) * probability, len(xArrStr) * teoreticalM, sum(mArr)
-           , criteriaSum])
+# pTab.add_row(["Sum"] + )
 
-pTab.set_cols_width(7 * [10])
+pTab.set_cols_width(11 * [10])
 printPTab = pTab.draw()
 
 print(printPTab)
-
-degreesOfFreedom = len(xArr) - 3
-signiflvl        = 0.05
-chisqcr          = 1.635
-
-print("Chi[b]^2 = %(chibs)f\nk = %(degoffr)f\nalpha = %(signiflvl)f\nChi[cr]^2 = %(chicrs)f"
-      % { "chibs" : criteriaSum, "degoffr" : degreesOfFreedom, "signiflvl" : signiflvl, "chicrs" : chisqcr })
